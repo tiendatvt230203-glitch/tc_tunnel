beedd910dbaf5f14f45960abdab5efa0a0169c81
tiendat2302 <tiendatvt230203@gmail>
2025-12-17 17:31:15 +0700

    update

--- loader.c
+++ loader.c
@@ -24,16 +24,13 @@ static int get_ifindex(const char *ifname) {
 static int get_mac(const char *ifname, __u8 *mac) {
     int fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (fd < 0) return -1;
-
     struct ifreq ifr;
     memset(&ifr, 0, sizeof(ifr));
     strncpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
-
     if (ioctl(fd, SIOCGIFHWADDR, &ifr) < 0) {
         close(fd);
         return -1;
     }
-
     memcpy(mac, ifr.ifr_hwaddr.sa_data, 6);
     close(fd);
     return 0;
@@ -42,19 +39,62 @@ static int get_mac(const char *ifname, __u8 *mac) {
 static int get_ip(const char *ifname, __u32 *ip) {
     int fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (fd < 0) return -1;
-
     struct ifreq ifr;
     memset(&ifr, 0, sizeof(ifr));
     strncpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
-
     if (ioctl(fd, SIOCGIFADDR, &ifr) < 0) {
         close(fd);
         return -1;
     }
-
     struct sockaddr_in *addr = (struct sockaddr_in *)&ifr.ifr_addr;
     *ip = addr->sin_addr.s_addr;
     close(fd);
+    return 0;
+}
+
+static int get_gateway_mac(const char *ifname, __u8 *mac) {
+    memset(mac, 0xff, 6);
+    return 0;
+}
+
+static int set_mtu(const char *ifname, int mtu) {
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd < 0) return -1;
+
+    struct ifreq ifr;
+    memset(&ifr, 0, sizeof(ifr));
+    strncpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
+    ifr.ifr_mtu = mtu;
+
+    int ret = ioctl(fd, SIOCSIFMTU, &ifr);
+    close(fd);
+    return ret;
+}
+
+static int parse_cidr(const char *cidr, __u32 *subnet, __u32 *mask) {
+    char ip_str[32];
+    int prefix_len;
+
+    if (sscanf(cidr, "%31[^/]/%d", ip_str, &prefix_len) != 2) {
+        return -1;
+    }
+
+    if (prefix_len < 0 || prefix_len > 32) {
+        return -1;
+    }
+
+    struct in_addr addr;
+    if (inet_pton(AF_INET, ip_str, &addr) != 1) {
+        return -1;
+    }
+
+    if (prefix_len == 0) {
+        *mask = 0;
+    } else {
+        *mask = htonl(~((1U << (32 - prefix_len)) - 1));
+    }
+
+    *subnet = addr.s_addr & *mask;
     return 0;
 }
 
@@ -63,13 +103,8 @@ static int attach_tc(int ifindex, int fd, int ingress) {
         .ifindex = ifindex,
         .attach_point = ingress ? BPF_TC_INGRESS : BPF_TC_EGRESS,
     );
-
     bpf_tc_hook_create(&hook);
-
-    DECLARE_LIBBPF_OPTS(bpf_tc_opts, opts,
-        .prog_fd = fd,
-    );
-
+    DECLARE_LIBBPF_OPTS(bpf_tc_opts, opts, .prog_fd = fd);
     return bpf_tc_attach(&hook, &opts);
 }
 
@@ -78,7 +113,6 @@ static void detach_tc(int ifindex, int ingress) {
         .ifindex = ifindex,
         .attach_point = ingress ? BPF_TC_INGRESS : BPF_TC_EGRESS,
     );
-
     bpf_tc_hook_destroy(&hook);
 }
 
@@ -89,12 +123,16 @@ int main(int argc, char **argv) {
     }
 
     FILE *fp = fopen(argv[1], "r");
-    if (!fp) return 1;
+    if (!fp) {
+        perror("fopen");
+        return 1;
+    }
 
     char local_if[32] = {0};
-    struct wan_info wans[MAX_WAN];
+    struct wan_cfg wans[MAX_WAN];
     int wan_count = 0;
-
+    __u32 remote_subnet = 0;
+    __u32 remote_mask = 0;
     memset(wans, 0, sizeof(wans));
 
     char line[256];
@@ -107,6 +145,19 @@ int main(int argc, char **argv) {
         if (strcmp(cmd, "local") == 0) {
             char *ifname = strtok(NULL, " \t\n");
             if (ifname) strncpy(local_if, ifname, sizeof(local_if) - 1);
+        } else if (strcmp(cmd, "remote") == 0) {
+            char *cidr = strtok(NULL, " \t\n");
+            if (cidr) {
+                if (parse_cidr(cidr, &remote_subnet, &remote_mask) == 0) {
+                    struct in_addr addr;
+                    addr.s_addr = remote_subnet;
+                    printf("[CONFIG] Remote subnet: %s", inet_ntoa(addr));
+                    addr.s_addr = remote_mask;
+                    printf("/%d\n", __builtin_popcount(ntohl(remote_mask)));
+                } else {
+                    fprintf(stderr, "[ERROR] Invalid remote CIDR: %s\n", cidr);
+                }
+            }
         } else if (strcmp(cmd, "wan") == 0) {
             char *local_part = strtok(NULL, " \t\n");
             char *peer_part = strtok(NULL, " \t\n");
@@ -120,15 +171,22 @@ int main(int argc, char **argv) {
                 if (sscanf(local_part, "%31[^:]:%d", ifname, &port) == 2 &&
                     sscanf(peer_part, "%31[^:]:%d", peer_ip, &peer_port) == 2) {
 
-                    struct wan_info *w = &wans[wan_count];
+                    struct wan_cfg *w = &wans[wan_count];
                     w->ifindex = get_ifindex(ifname);
                     w->local_port = port;
                     w->peer_port = peer_port;
                     inet_pton(AF_INET, peer_ip, &w->peer_ip);
                     get_ip(ifname, &w->local_ip);
                     get_mac(ifname, w->local_mac);
-                    memset(w->peer_mac, 0xff, 6);
+                    get_gateway_mac(ifname, w->gw_mac);
                     w->active = 1;
+
+                    struct in_addr addr;
+                    addr.s_addr = w->local_ip;
+                    printf("[CONFIG] WAN %d: %s port %d -> ", wan_count, ifname, port);
+                    addr.s_addr = w->peer_ip;
+                    printf("%s:%d\n", inet_ntoa(addr), peer_port);
+
                     wan_count++;
                 }
             }
@@ -136,7 +194,15 @@ int main(int argc, char **argv) {
     }
     fclose(fp);
 
-    if (local_if[0] == 0 || wan_count == 0) return 1;
+    if (local_if[0] == 0 || wan_count == 0) {
+        fprintf(stderr, "[ERROR] Missing local interface or WAN config\n");
+        return 1;
+    }
+
+    if (remote_subnet == 0 && remote_mask == 0) {
+        fprintf(stderr, "[ERROR] Missing remote subnet (e.g., remote 192.168.2.0/24)\n");
+        return 1;
+    }
 
     struct bpf_object *encap_obj = bpf_object__open_file("tc_encap.bpf.o", NULL);
     if (!encap_obj) return 1;
@@ -154,24 +220,33 @@ int main(int argc, char **argv) {
     int decap_fd = bpf_program__fd(decap_prog);
 
     int wan_map_fd = bpf_object__find_map_fd_by_name(encap_obj, "wan_map");
-    int flow_map_fd = bpf_object__find_map_fd_by_name(encap_obj, "flow_map");
     int cfg_map_fd = bpf_object__find_map_fd_by_name(encap_obj, "cfg_map");
-    int lb_counter_fd = bpf_object__find_map_fd_by_name(encap_obj, "lb_counter");
-
+    int lb_map_fd = bpf_object__find_map_fd_by_name(encap_obj, "lb_map");
     int decap_cfg_fd = bpf_object__find_map_fd_by_name(decap_obj, "cfg_map");
     int decap_wan_fd = bpf_object__find_map_fd_by_name(decap_obj, "wan_map");
 
     struct tunnel_cfg cfg = {
         .local_ifindex = get_ifindex(local_if),
         .wan_count = wan_count,
+        .remote_subnet = remote_subnet,
+        .remote_mask = remote_mask,
     };
+
+    printf("[CONFIG] Local interface: %s (ifindex %d)\n", local_if, cfg.local_ifindex);
+    printf("[CONFIG] WAN count: %d\n", wan_count);
+
+    if (set_mtu(local_if, LOCAL_MTU) == 0) {
+        printf("[CONFIG] Set %s MTU to %d\n", local_if, LOCAL_MTU);
+    } else {
+        fprintf(stderr, "[WARN] Failed to set MTU on %s (need root)\n", local_if);
+    }
 
     __u32 key0 = 0;
     bpf_map_update_elem(cfg_map_fd, &key0, &cfg, BPF_ANY);
     bpf_map_update_elem(decap_cfg_fd, &key0, &cfg, BPF_ANY);
 
-    __u32 counter = 0;
-    bpf_map_update_elem(lb_counter_fd, &key0, &counter, BPF_ANY);
+    struct lb_state lb = { .wan_idx = 0 };
+    bpf_map_update_elem(lb_map_fd, &key0, &lb, BPF_ANY);
 
     for (int i = 0; i < wan_count; i++) {
         __u32 idx = i;
@@ -180,18 +255,25 @@ int main(int argc, char **argv) {
     }
 
     int local_ifindex = get_ifindex(local_if);
-    attach_tc(local_ifindex, encap_fd, 0);
+
+    printf("[ATTACH] tc_encap on %s (egress)\n", local_if);
+    if (attach_tc(local_ifindex, encap_fd, 0) < 0) {
+        fprintf(stderr, "[ERROR] Failed to attach encap on %s\n", local_if);
+    }
 
     for (int i = 0; i < wan_count; i++) {
-        attach_tc(wans[i].ifindex, decap_fd, 1);
+        printf("[ATTACH] tc_decap on ifindex %d (ingress)\n", wans[i].ifindex);
+        if (attach_tc(wans[i].ifindex, decap_fd, 1) < 0) {
+            fprintf(stderr, "[ERROR] Failed to attach decap on ifindex %d\n", wans[i].ifindex);
+        }
     }
 
     signal(SIGINT, sig_handler);
     signal(SIGTERM, sig_handler);
 
-    while (running) {
-        sleep(1);
-    }
+    printf("\n[RUNNING] Tunnel active. Press Ctrl+C to stop.\n");
+
+    while (running) sleep(1);
 
     detach_tc(local_ifindex, 0);
     for (int i = 0; i < wan_count; i++) {
@@ -200,6 +282,5 @@ int main(int argc, char **argv) {
 
     bpf_object__close(encap_obj);
     bpf_object__close(decap_obj);
-
     return 0;
 }
--- tc_common.h
+++ tc_common.h
@@ -1,9 +1,18 @@
 #ifndef TC_COMMON_H
 #define TC_COMMON_H
 
-#define TUNNEL_PORT         5001
-#define TUNNEL_MAGIC        0x544E4C
+#define WAN_MTU             1500
+#define IP_HEADER_SIZE      20
+#define UDP_HEADER_SIZE     8
+#define ETH_HEADER_SIZE     14
+
+#define TUNNEL_HEADER_USED  17
+#define TUNNEL_HEADER_RSVD  30
 #define TUNNEL_HEADER_SIZE  47
+
+#define MAX_FRAME_SIZE      1425
+#define LOCAL_MTU           1411
+
 #define MAX_WAN             8
 #define MAX_FLOWS           4096
 
@@ -17,18 +26,19 @@ struct tunnel_hdr {
     __u32 seq;
     __u32 len;
     __u32 crc32;
-    __u8  reserved[30];
+    __u8  reserved[TUNNEL_HEADER_RSVD];
 } __attribute__((packed));
 
-struct wan_info {
+struct wan_cfg {
     __u32 local_ip;
     __u32 peer_ip;
     __u16 local_port;
     __u16 peer_port;
     __u32 ifindex;
     __u8  local_mac[6];
-    __u8  peer_mac[6];
+    __u8  gw_mac[6];
     __u8  active;
+    __u8  pad[3];
 };
 
 struct flow_key {
@@ -37,17 +47,24 @@ struct flow_key {
     __u16 src_port;
     __u16 dst_port;
     __u8  protocol;
-} __attribute__((packed));
+    __u8  pad[3];
+};
 
-struct flow_entry {
-    __u32 wan_index;
-    __u32 seq;
+struct flow_state {
+    __u32 wan_idx;
+    __u32 tx_seq;
     __u64 last_seen;
 };
 
 struct tunnel_cfg {
     __u32 local_ifindex;
     __u32 wan_count;
+    __u32 remote_subnet;
+    __u32 remote_mask;
+};
+
+struct lb_state {
+    __u32 wan_idx;
 };
 
 #endif
